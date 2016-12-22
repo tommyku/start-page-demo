6f68f911136b66b3bd2aa31169928e8e758619f1
Tommy Ku <tommyku@eventxtra.com>
2016-12-22 16:24:32 +0800

    ADD: server

---
+++ web-proxy/server.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+require 'sinatra'
+require 'sinatra/cross_origin'
+require 'awesome_print'
+require 'forecast_io'
+require 'json'
+
+configure do
+  enable :cross_origin
+  set :port, 1080
+  set :bind, '0.0.0.0'
+end
+
+# Dark Sky API, get your API key from:
+# https://darksky.net/dev/
+ForecastIO.api_key = 'YOUR_DARK_SKY_API_KEY'
+
+get '/weather' do
+  content_type :json
+  ForecastIO.forecast(22.3986, 114.1929, params: {exclude: 'minutely,daily,alerts,flags', units: :si, lang: 'zh-tw'}).to_json
+end
